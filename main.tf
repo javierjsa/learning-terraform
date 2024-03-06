0b03ddeaf3c679116de4dcae3c7c2f1d24bbc3fa
jsaez <javierjsa@gmail.com>
2024-03-06 14:41:19 +0100

    Fixed declaration

--- main.tf
+++ main.tf
@@ -63,7 +63,7 @@ module "alb" {
         target_type      = "instance"
         targets = {
           my_target = {
-              target_id = module.aws_instance.blog.id
+              target_id = aws_instance.blog.id
               port = 80
           }
       }
@@ -96,40 +96,3 @@ module "security-group" {
   egress_cidr_blocks = ["0.0.0.0/0"]
 	}
 
-/*
-resource "aws_security_group" "blog" {
-  name        = "blog" // name in aws console
-  description = "Allow web traffic in. Allow everything out"
-  vpc_id      = data.aws_vpc.default.id
-}
-
-resource "aws_security_group_rule" "blog_http_in" {
-  type              = "ingress"
-  from_port         = 80
-  to_port           = 80
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.blog.id
-}
-
-resource "aws_security_group_rule" "blog_https_in" {
-  type              = "ingress"
-  from_port         = 443
-  to_port           = 443
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.blog.id
-}
-
-resource "aws_security_group_rule" "blog_http_out" {
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.blog.id
-}
-*/
